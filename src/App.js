d63dae08da0100af8c5a002cb80bbd4c947e5170
ivanhreskiv <ivanhreskiv@gmai.com>
2017-08-15 16:33:53 +0300

    Droped app down into componnets.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,121 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="main">
+          <ConversationList />
+          <MessageList />
         </div>
-        <p className="App-intro">
-          Hi Ivan!!!.
-        </p>
-      </div>
     );
   }
+}
+
+class ConversationList extends Component{
+  render() {
+      return (
+          <div className="listings">
+              <ChannelsList
+                  unread="25"
+                  channel_name="admim"
+              />
+              <DirectMessagesList
+                  participant="Prticipant1"
+                  unread_messages="10"
+              />
+          </div>
+      );
+    }
+}
+
+
+class ChannelsList extends Component {
+    render() {
+        return (
+            <div className="listings_channels">
+                <h2 className="listings_header">Channels</h2>
+                <ul className="channel_list">
+                    <li className="channel active">
+                        <a className="channel_name">
+                            <span className="unread">
+                                {this.props.unread}
+                            </span>
+                            <span><span className="prefix">#</span>
+                                {this.props.channel_name}
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+class DirectMessagesList extends Component {
+    render() {
+        return (
+            <div className="listings_direct-messages">
+                <h2 className="listings_header">Direct Messages</h2>
+                <ul className="channel_list">
+                    <li className="channel">
+                        <a className="channel_name">
+                            <span className="unread">
+                                {this.props.unread_messages}
+                            </span>
+                            <span><span className="prefix"></span>
+                                {this.props.participant}
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+class MessageList extends Component {
+  render() {
+    return (
+          <div className="message-history">
+              <Message
+                  message="Hi, Ivan! It is me"
+                  sent_at="11 AM"
+                  sender="Chika"
+              />
+              <Message
+                  message="Hi, Ivan! It is me second time"
+                  sent_at="11 AM"
+                  sender="Chika"
+              />
+              <Message
+                  message="Hi, Ivan! It is me third time"
+                  sent_at="12 AM"
+                  sender="Chika"
+              />
+         </div>
+        );
+  }
+}
+
+class Message extends Component {
+    render() {
+        return (
+            <div className="message">
+                <a className="message_profile-pic" href=""></a>
+                <a className="message_username" href="">
+                    {this.props.sender}
+                </a>
+                <span className="message_timestamp">
+                    {this.props.sent_at}
+                </span>
+                <span className="message_star"></span>
+                <span className="message_content">
+                    {this.props.message}
+                </span>
+            </div>
+
+        );
+    }
 }
 
 export default App;
